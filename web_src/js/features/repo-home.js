d6c6f0aeaa7f221186e94f735adb54e95335307d
ybyang <ybyang7@iflytek.com>
2023-03-25 23:04:30 +0800

    feat: topic support : _ space

--- web_src/js/features/repo-home.js
+++ web_src/js/features/repo-home.js
@@ -168,7 +168,7 @@ export function initRepoTopicBar() {
         rules: [
           {
             type: 'validateTopic',
-            value: /^[a-z0-9][a-z0-9-]{0,35}$/,
+            value: /^[a-z0-9][a-z0-9-:_. ]{0,35}$/,
             prompt: topicPrompts.formatPrompt
           },
           {
